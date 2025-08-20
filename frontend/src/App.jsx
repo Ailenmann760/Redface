da0a2dd42988ae40bd38e29098d2899c4da1a0cf
Richard Joseph <163758054+Ailenmann760@users.noreply.github.com>
2025-08-20 23:26:17 +0100

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import ProfileSetup from './pages/ProfileSetup';
 import Dating from './pages/Dating';
-// REMOVED: import Feed from './pages/Feed';
-// REMOVED: import Explore from './pages/Explore';
-// REMOVED: import Messaging from './pages/Messaging';
+import Feed from './pages/Feed';
+import Explore from './pages/Explore';
+import Messaging from './pages/Messaging';
 import Settings from './pages/Settings';
 import Navbar from './components/Navbar';
 import { motion } from 'framer-motion';
@@ -25,9 +25,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/profile-setup" element={<ProfileSetup />} />
           <Route path="/dating" element={<Dating />} />
-          {/* REMOVED: <Route path="/feed" element={<Feed />} /> */}
-          {/* REMOVED: <Route path="/explore" element={<Explore />} /> */}
-          {/* REMOVED: <Route path="/messages" element={<Messaging />} /> */}
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/messages" element={<Messaging />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </motion.div>
