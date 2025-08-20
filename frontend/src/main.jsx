383f9a8efad8bc0a64dc828999f51f990ad783f1
Richard Joseph <163758054+Ailenmann760@users.noreply.github.com>
2025-08-20 02:07:23 +0100

    main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import App from './App.jsx';
+import './index.css';
+import { Toaster } from 'sonner';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+    <Toaster />
+  </React.StrictMode>
+);
