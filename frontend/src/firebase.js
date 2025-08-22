b767260b1a47dd8edca3763bb611ca729d6f4f39
Richard Joseph <163758054+Ailenmann760@users.noreply.github.com>
2025-08-22 14:14:51 -0500

    firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,25 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBksqBwPLKZNR7XjcS1r5SDDqLasDpdyJM",
+  authDomain: "redface-773bc.firebaseapp.com",
+  projectId: "redface-773bc",
+  storageBucket: "redface-773bc.firebasestorage.app",
+  messagingSenderId: "152641062731",
+  appId: "1:152641062731:web:c0e7a5c6991da5c0bd4ace",
+  measurementId: "G-KC72XMZCXH"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const storage = getStorage(app);
